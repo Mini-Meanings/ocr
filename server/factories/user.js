07147e1c9879802a058f7c818abc07028b9af838
beiyannanfei <18810776836@163.com>
2018-03-04 14:41:55 +0800

    add debug log
    add debug log

--- server/factories/user.js
+++ server/factories/user.js
@@ -3,6 +3,7 @@
  * 依赖注入模块
  */
 const MD5 = require("md5");
+const logger = require("../utils/log")(__filename);
 
 module.exports = function () {
 	return function (req, res, next) {
@@ -10,6 +11,7 @@ module.exports = function () {
 			if (req.query.u === "t") {
 				return next(null, Math.floor(+new Date() / 100000).toString());
 			}
+			logger.warn("===== 获取authid失败");
 			return next(null, null);
 		}
 		return next(null, MD5(req.headers["authid"]));
